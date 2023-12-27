acd7aa118e437df371fc6dcca801c340ee237f68
quaqduy <duycnttdo@gmail.com>
2023-12-27 23:48:05 +0700

    add favicon

--- CV/MVC/views/layouts/pageLayout.php
+++ CV/MVC/views/layouts/pageLayout.php
@@ -8,6 +8,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="/MVCPHP/CV/MVC/public/css/css_layouts/pageLayout.css">        
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha384-...." crossorigin="anonymous">
+        <link rel="shortcut icon" type="image/png" href="/MVCPHP/CV/MVC/public/img/logo/favicon.png"/>
     </head>
     <body>
 
